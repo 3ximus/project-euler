df4b7c1854e6a7a3feaaa263dc34f0eae3ae139f
eximus <fabio_r11@hotmail.com>
2017-10-29 16:48:49 +0000

    finished 14

--- p14.py
+++ p14.py
@@ -1,14 +1,9 @@
 target = 1000000
-cache = [None] * (target + 1) # should be enough
-cache_size = target
+cache = {} # should be enough
 
 def collatz(n):
 	if n == 1: return 1
-	try:
-		if cache[n]: return cache[n]
-	except IndexError:
-		cache.extend([None] * (n - cache_size + 1))
-		cache_size = n + 1
+	if n in cache: return cache[n]
 	if n % 2 == 0: new = collatz(n//2) + 1
 	else: new = collatz(3*n +1) + 1
 	cache[n] = new
@@ -20,6 +15,6 @@ for i in range(target,1,-1):
 	if m_chain < collatz(i):
 		m_chain = collatz(i)
 		starting_n = i
-print(starting_n)
+print(starting_n, m_chain)
 
 
