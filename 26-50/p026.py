14b377aad7948634b03c67cd3a94c565851ea3c7
eximus <fabio_r11@hotmail.com>
2017-12-11 21:37:21 +0000

    26

--- 26-50/p026.py
+++ 26-50/p026.py
@@ -1,11 +1,22 @@
-p10 = []
-for i in range(20): # generate a powers of 10 list
-	p10.append(10**i)
-pminus10 = []
-for i in range(20): # generate a negative powers of 10 list
-	pminus10.append(10**-i)
+def find_cycle(d):
+	rlist = []
+	qlist_len = 0
+	remainder = 1
+	while remainder:
+		remainder = remainder % d # allows to generate infinited decimal numbers
+		if remainder in rlist:
+			return qlist_len - rlist.index(remainder)
+		rlist.append(remainder)
+		remainder *= 10
+		qlist_len+=1
+	return 0
 
-def digit_at(x,p,n=1): # digit at position p in number x, n is the amount of digits returned (backwards)
-	return int((x / pminus10[p]) % p10[n])
-
-
+d = 1000
+many_digits = 0
+max_d = 0
+for n in range(2, d):
+	val = find_cycle(n)
+	if  val > many_digits:
+		many_digits = val
+		max_d = n
+print(max_d)
