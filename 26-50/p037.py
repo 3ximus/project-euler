a9294d65af179dd02361bd6d096ea0a30b30763d
eximus <fabio_r11@hotmail.com>
2017-12-27 17:24:08 +0000

    progress on 37

--- 26-50/p037.py
+++ 26-50/p037.py
@@ -1,3 +1,4 @@
+
 def is_prime(n):
 	if n in primes: return True # speedup
 	for i in range(2,int(n**0.5+1)):
@@ -8,21 +9,23 @@ def get_truncation(n):
 	ns = str(n)
 	for i in range(1, len(ns)): # left to right
 		yield int(ns[:i])
+		yield int(ns[i:])
 
-forbidden_digits = set('1234567890') - set('1379')
+forbidden_digits = set('1234567890') - set('379')
 primes = set()
 count = 0
 sumation = 0
 i = 8
 while count != 11:
+	i += 1
 	if any([x in str(i) for x in forbidden_digits]): # sleeve trick xD
 		continue
 	if is_prime(i) and all([is_prime(x) for x in get_truncation(i)]):
+		print(i)
 		primes.add(i)
 		primes |= set(get_truncation(i))
 		count += 1
 		sumation += i
-	i += 1
 
 print(sumation)
 
