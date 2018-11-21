99f83967118bcc5bfe578c00b74ecf431c678414
eximus <fabio_r11@hotmail.com>
2018-11-21 21:05:09 +0000

    faster 43

--- 26-50/p043.py
+++ 26-50/p043.py
@@ -1,13 +1,10 @@
 from itertools import permutations
 
-sumation = 0
 primes = (2, 3, 5, 7 , 11, 13, 17)
-for i in permutations('0123456789'):
-	number = ''.join(i)
-	failed = False
-	for c in range(1,8):
+def is_valid(number):
+	for c in range(7,0,-1):
 		if int(number[c:c+3]) % primes[c - 1] != 0:
-			failed = True
-			break
-	if not failed: sumation += int(number)
-print(sumation)
+			return False
+	return True
+
+print(sum([int(''.join(i)) for i in permutations('0123456789') if is_valid(''.join(i))]))
